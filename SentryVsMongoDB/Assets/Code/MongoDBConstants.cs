6b0e50563399e09f23afcaaf82f1578b23d417ae
mraue <martin.raue@gmail.com>
2023-04-05 09:41:56 +0200

    Adds MongoDBConstants

---
+++ SentryVsMongoDB/Assets/Code/MongoDBConstants.cs
@@ -0,0 +1,6 @@
+public static class MongoDBConstants
+{
+    public static readonly string ConnectionString = "mongodb+srv://{0}:{1}@cluster0.abc.mongodb.net/?retryWrites=true&w=majority";
+    public static readonly string MongoDBUserName = "my-user";
+    public static readonly string MongoDBPassword = "my-pw";
+}
